5173511146fcd5806261ddce15a45a8b147b2162
Anson Hoyt <ahoyt@tiu11.org>
2019-01-08 13:04:31 -0500

    Sync the CSV changes as we go. Fixes the CSV file being empty when we quit the program.

--- functions_example.py
+++ functions_example.py
@@ -9,10 +9,13 @@ def read_temp():
 	now = time().strftime('%I:%M:%S %p') # 2:05:01 PM
 	temp = sensor.get_temperature()
 	return [now, temp]
+
 with open('data.csv', 'w') as f:
 	writer = csv.writer(f)
 	writer.write_row(['Time', 'Temperature'])
+
 	while True:
 		writer.write_row(read_temp())
+		f.flush() # finish up the write
 		print('row was written')
 		sleep(1)
